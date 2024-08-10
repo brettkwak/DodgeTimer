6416f637c7e79dc65d7063fd72663cee47f33de4
BKwak28 <brettkwak@gmail.com>
2024-08-10 21:12:25 +0900

    Added save/load feature

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,16 +1,26 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include <QDateTime>
+#include <QFile>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    QFile file("record.txt");
+    file.open(QFile::ReadOnly|QFile::Text);
+    QTextStream OpenFile(&file);
+    QString ConfigText;
+    ConfigText=OpenFile.readLine();
+
     ui->setupUi(this);
+
+    ui->TLabel1->setText(ConfigText);
 }
 
 MainWindow::~MainWindow()
 {
+    QFile("A.txt");
     delete ui;
 }
 
@@ -20,5 +30,10 @@ void MainWindow::on_TButton1_clicked()
     QDateTime timeDate = QDateTime::currentDateTime();
     QString timeString = timeDate.toString("yyyy.MM.dd hh:mm:ss");
     ui->TLabel1->setText(timeString);
+
+    QFile file("record.txt");
+    file.open(QIODevice::WriteOnly | QIODevice::Text);
+    QTextStream out(&file);
+    out << timeString;
 }
 
