5c996f167ffce67aadf3d4555c6a858be11cf589
Kwak <brettkwak@gmail.com>
2024-10-04 03:00:19 +0900

    feat : supports 5 accounts

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,14 +11,32 @@ MainWindow::MainWindow(QWidget *parent)
     QFile file("record.txt");
     file.open(QFile::ReadOnly|QFile::Text);
     QTextStream OpenFile(&file);
+
+
     stringDodgeTime1 = OpenFile.readLine();
     dodgeCount1 = OpenFile.readLine().toInt();
+    stringDodgeTime2 = OpenFile.readLine();
+    dodgeCount2 = OpenFile.readLine().toInt();
+    stringDodgeTime3 = OpenFile.readLine();
+    dodgeCount3 = OpenFile.readLine().toInt();
+    stringDodgeTime4 = OpenFile.readLine();
+    dodgeCount4 = OpenFile.readLine().toInt();
+    stringDodgeTime5 = OpenFile.readLine();
+    dodgeCount5 = OpenFile.readLine().toInt();
 
     lastDodgeTime1 = QDateTime::fromString(stringDodgeTime1, "yyyy.MM.dd HH:mm:ss");
+    lastDodgeTime2 = QDateTime::fromString(stringDodgeTime2, "yyyy.MM.dd HH:mm:ss");
+    lastDodgeTime3 = QDateTime::fromString(stringDodgeTime3, "yyyy.MM.dd HH:mm:ss");
+    lastDodgeTime4 = QDateTime::fromString(stringDodgeTime4, "yyyy.MM.dd HH:mm:ss");
+    lastDodgeTime5 = QDateTime::fromString(stringDodgeTime5, "yyyy.MM.dd HH:mm:ss");
 
     ui->setupUi(this);
 
-    updateLabels();
+    updateLabels1();
+    updateLabels2();
+    updateLabels3();
+    updateLabels4();
+    updateLabels5();
 
     QTimer *timer = new QTimer(this);
     QObject::connect(timer, SIGNAL(timeout()), this, SLOT(update()));
@@ -30,6 +48,36 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+    QFile file("record.txt");
+    file.open(QIODevice::WriteOnly | QIODevice::Text);
+    QTextStream out(&file);
+    out << stringDodgeTime1 << "\n" << dodgeCount1 << "\n";
+    out << stringDodgeTime2 << "\n" << dodgeCount2 << "\n";
+    out << stringDodgeTime3 << "\n" << dodgeCount3 << "\n";
+    out << stringDodgeTime4 << "\n" << dodgeCount4 << "\n";
+    out << stringDodgeTime5 << "\n" << dodgeCount5 << "\n";
+}
+
+void MainWindow::update(){
+    qDebug() << "Update...";
+
+    currentTime = QDateTime::currentDateTime();
+
+    updateTime1();
+    updateTime2();
+    updateTime3();
+    updateTime4();
+    updateTime5();
+
+    updateLabels1();
+    updateLabels2();
+    updateLabels3();
+    updateLabels4();
+    updateLabels5();
+}
+
 
 void MainWindow::on_TButton1_clicked()
 {
@@ -37,51 +85,184 @@ void MainWindow::on_TButton1_clicked()
     lastDodgeTime1 = QDateTime::currentDateTime();
     stringDodgeTime1 = lastDodgeTime1.toString("yyyy.MM.dd hh:mm:ss");
 
-    updateLabels();
+    updateLabels1();
 }
 
-void MainWindow::closeEvent(QCloseEvent *event)
+void MainWindow::on_TButton2_clicked()
 {
-    QFile file("record.txt");
-    file.open(QIODevice::WriteOnly | QIODevice::Text);
-    QTextStream out(&file);
-    out << stringDodgeTime1 << "\n" << dodgeCount1;
+    dodgeCount2++;
+    lastDodgeTime2 = QDateTime::currentDateTime();
+    stringDodgeTime2 = lastDodgeTime2.toString("yyyy.MM.dd hh:mm:ss");
+
+    updateLabels2();
 }
 
-void MainWindow::on_Button_Reset_clicked()
+void MainWindow::on_TButton3_clicked()
+{
+    dodgeCount3++;
+    lastDodgeTime3 = QDateTime::currentDateTime();
+    stringDodgeTime3 = lastDodgeTime3.toString("yyyy.MM.dd hh:mm:ss");
+
+    updateLabels3();
+}
+
+void MainWindow::on_TButton4_clicked()
+{
+    dodgeCount4++;
+    lastDodgeTime4 = QDateTime::currentDateTime();
+    stringDodgeTime4 = lastDodgeTime4.toString("yyyy.MM.dd hh:mm:ss");
+
+    updateLabels4();
+}
+
+void MainWindow::on_TButton5_clicked()
+{
+    dodgeCount5++;
+    lastDodgeTime5 = QDateTime::currentDateTime();
+    stringDodgeTime5 = lastDodgeTime5.toString("yyyy.MM.dd hh:mm:ss");
+
+    updateLabels5();
+}
+
+
+void MainWindow::updateLabels1(){
+    ui->CLabel1->setText(QString::number(dodgeCount1));
+    ui->TLabel1->setText(stringDodgeTime1);
+}
+
+void MainWindow::updateLabels2(){
+    ui->CLabel2->setText(QString::number(dodgeCount2));
+    ui->TLabel2->setText(stringDodgeTime2);
+}
+
+void MainWindow::updateLabels3(){
+    ui->CLabel3->setText(QString::number(dodgeCount3));
+    ui->TLabel3->setText(stringDodgeTime3);
+}
+
+void MainWindow::updateLabels4(){
+    ui->CLabel4->setText(QString::number(dodgeCount4));
+    ui->TLabel4->setText(stringDodgeTime4);
+}
+
+void MainWindow::updateLabels5(){
+    ui->CLabel5->setText(QString::number(dodgeCount5));
+    ui->TLabel5->setText(stringDodgeTime5);
+}
+
+
+void MainWindow::on_RButton1_clicked()
 {
     stringDodgeTime1 = "No Penalty";
     dodgeCount1 = 0;
-    updateLabels();
+    updateLabels1();
 }
 
-void MainWindow::update(){
-    qDebug() << "Update...";
+void MainWindow::on_RButton2_clicked()
+{
+    stringDodgeTime2 = "No Penalty";
+    dodgeCount2 = 0;
+    updateLabels2();
+}
 
-    currentTime = QDateTime::currentDateTime();
-    timeDifference = lastDodgeTime1.secsTo(currentTime);
-    qDebug() << "String1 :" << stringDodgeTime1;
-    qDebug() << "Differ1 :" <<timeDifference;
-    qDebug() << "Count1 :" << dodgeCount1;
-    if (timeDifference > 43200){
+void MainWindow::on_RButton3_clicked()
+{
+    stringDodgeTime3 = "No Penalty";
+    dodgeCount3 = 0;
+    updateLabels3();
+}
+
+void MainWindow::on_RButton4_clicked()
+{
+    stringDodgeTime4 = "No Penalty";
+    dodgeCount4 = 0;
+    updateLabels4();
+}
+
+void MainWindow::on_RButton5_clicked()
+{
+    stringDodgeTime5 = "No Penalty";
+    dodgeCount5 = 0;
+    updateLabels5();
+}
+
+
+void MainWindow::updateTime1(){
+    timeDifference1 = lastDodgeTime1.secsTo(currentTime);
+    if (timeDifference1 > 43200){
         dodgeCount1--;
 
         lastDodgeTime1 = lastDodgeTime1.addSecs(43200);
-        qDebug() << "Reset!";
+        qDebug() << "Reset 1!";
     }
-
     if (dodgeCount1 == 0){
         stringDodgeTime1 = "No Penalty";
     }
     else {
         stringDodgeTime1 = lastDodgeTime1.toString("yyyy.MM.dd hh:mm:ss");
     }
-
-    updateLabels();
-
 }
 
-void MainWindow::updateLabels(){
-    ui->DLabel1->setText(QString::number(dodgeCount1));
-    ui->TLabel1->setText(stringDodgeTime1);
+void MainWindow::updateTime2(){
+    timeDifference2 = lastDodgeTime2.secsTo(currentTime);
+    if (timeDifference2 > 43200){
+        dodgeCount2--;
+
+        lastDodgeTime2 = lastDodgeTime2.addSecs(43200);
+        qDebug() << "Reset 2!";
+    }
+    if (dodgeCount2 == 0){
+        stringDodgeTime2 = "No Penalty";
+    }
+    else {
+        stringDodgeTime2 = lastDodgeTime2.toString("yyyy.MM.dd hh:mm:ss");
+    }
+}
+
+void MainWindow::updateTime3(){
+    timeDifference3 = lastDodgeTime3.secsTo(currentTime);
+    if (timeDifference3 > 43200){
+        dodgeCount3--;
+
+        lastDodgeTime3 = lastDodgeTime3.addSecs(43200);
+        qDebug() << "Reset 3!";
+    }
+    if (dodgeCount3 == 0){
+        stringDodgeTime3 = "No Penalty";
+    }
+    else {
+        stringDodgeTime3 = lastDodgeTime3.toString("yyyy.MM.dd hh:mm:ss");
+    }
+}
+
+void MainWindow::updateTime4(){
+    timeDifference4 = lastDodgeTime4.secsTo(currentTime);
+    if (timeDifference4 > 43200){
+        dodgeCount4--;
+
+        lastDodgeTime4 = lastDodgeTime4.addSecs(43200);
+        qDebug() << "Reset 4!";
+    }
+    if (dodgeCount4 == 0){
+        stringDodgeTime4 = "No Penalty";
+    }
+    else {
+        stringDodgeTime4 = lastDodgeTime4.toString("yyyy.MM.dd hh:mm:ss");
+    }
+}
+
+void MainWindow::updateTime5(){
+    timeDifference5 = lastDodgeTime5.secsTo(currentTime);
+    if (timeDifference5 > 43200){
+        dodgeCount5--;
+
+        lastDodgeTime5 = lastDodgeTime5.addSecs(43200);
+        qDebug() << "Reset 5!";
+    }
+    if (dodgeCount5 == 0){
+        stringDodgeTime5 = "No Penalty";
+    }
+    else {
+        stringDodgeTime5 = lastDodgeTime5.toString("yyyy.MM.dd hh:mm:ss");
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -20,23 +20,58 @@ public:
 
 private slots:
     void on_TButton1_clicked();
+    void on_TButton2_clicked();
+    void on_TButton3_clicked();
+    void on_TButton4_clicked();
+    void on_TButton5_clicked();
 
-    void on_Button_Reset_clicked();
     void update();
+
+    void on_RButton1_clicked();
+    void on_RButton2_clicked();
+    void on_RButton3_clicked();
+    void on_RButton4_clicked();
+    void on_RButton5_clicked();
 
 private:
     Ui::MainWindow *ui;
 
-    int dodgeCount1;
-
     void closeEvent(QCloseEvent *event);
-    void updateLabels();
 
-    QString timeString;
+    void updateLabels1();
+    void updateLabels2();
+    void updateLabels3();
+    void updateLabels4();
+    void updateLabels5();
+
+    void updateTime1();
+    void updateTime2();
+    void updateTime3();
+    void updateTime4();
+    void updateTime5();
 
     QDateTime currentTime;
-    int timeDifference;
+    int timeDifference1;
+    int timeDifference2;
+    int timeDifference3;
+    int timeDifference4;
+    int timeDifference5;
+
+    int dodgeCount1;
     QString stringDodgeTime1;
     QDateTime lastDodgeTime1;
+    int dodgeCount2;
+    QString stringDodgeTime2;
+    QDateTime lastDodgeTime2;
+    int dodgeCount3;
+    QString stringDodgeTime3;
+    QDateTime lastDodgeTime3;
+    int dodgeCount4;
+    QString stringDodgeTime4;
+    QDateTime lastDodgeTime4;
+    int dodgeCount5;
+    QString stringDodgeTime5;
+    QDateTime lastDodgeTime5;
+
 };
 #endif // MAINWINDOW_H
